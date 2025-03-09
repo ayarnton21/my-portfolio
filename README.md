04af02367d9af1f1b638ed7c69b7e6ccf104e5fb
Ashley Yarnton <ashley.yarnton@betfred.com>
2025-03-09 19:45:45 +0000

    fixing conlicts

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # my-portfolio
+
 An app that allows users to see my CV and the different projects I've worked on
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className=" text-blue-300">
+      <h1>Home</h1>
+    </div>
+  );
+};
+
+export default Home;
